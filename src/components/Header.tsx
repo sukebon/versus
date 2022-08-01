6dac014dd485dc2b86ca743dfcdb480b372d33ad
sukebon <39957486+sukebon@users.noreply.github.com>
2022-08-01 22:04:57 +0900

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { Box, Flex } from '@chakra-ui/react';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <Flex
+      alignItems='center'
+      p={3}
+      w='100%'
+      h={12}
+      backgroundColor='facebook.300'
+    >
+      header
+    </Flex>
+  );
+};
+
+export default Header;
